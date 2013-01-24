0bd5eaa6b4857d51764d8acfeec1cd90a79ef82e
Henrik Johansson <hencjo@gmail.com>
2013-01-24 20:32:25 +0100

    Added WWW-Authenticate responder to HttpBasicAuthenticator.

--- src/main/java/com/hencjo/summer/security/HttpBasicAuthenticator.java
+++ src/main/java/com/hencjo/summer/security/HttpBasicAuthenticator.java
@@ -1,8 +1,11 @@
 package com.hencjo.summer.security;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import com.hencjo.summer.security.api.Authenticator;
 import com.hencjo.summer.security.api.RequestMatcher;
+import com.hencjo.summer.security.api.Responder;
 import com.hencjo.summer.security.utils.Base64;
 import com.hencjo.summer.security.utils.Charsets;
 
@@ -10,9 +13,13 @@ public final class HttpBasicAuthenticator {
 	private final Authenticator authenticator;
 	private final SummerAuthenticatedUser summerAuthenticatedUser = new SummerAuthenticatedUser();
 	private final Base64 base64 = new Base64();
+	private final String realm;
 
-	public HttpBasicAuthenticator(Authenticator authenticator) {
+	public HttpBasicAuthenticator(Authenticator authenticator, String realm) {
+		if (realm == null) throw new IllegalArgumentException("Realm may not be null.");
+		if (realm.contains("\"")) throw new IllegalArgumentException("Realm may not contain quotes (\")");
 		this.authenticator = authenticator;
+		this.realm = realm;
 	}
 
 	public RequestMatcher authorizes() {
@@ -35,6 +42,22 @@ public final class HttpBasicAuthenticator {
 			@Override
 			public String describer() {
 				return "HttpBasic";
+			}
+		};
+	}
+
+	public Responder wwwAuthenticated() {
+		return new Responder() {
+			@Override
+			public ContinueOrRespond respond(HttpServletRequest request, HttpServletResponse response) throws IOException {
+				response.setHeader("WWW-Authenticate", "Basic realm=\""+realm+"\"");
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+				return ContinueOrRespond.RESPOND;
+			}
+
+			@Override
+			public String describer() {
+				return "WWW-Authenticate: Basic realm=\"" + realm + "\"";
 			}
 		};
 	}
