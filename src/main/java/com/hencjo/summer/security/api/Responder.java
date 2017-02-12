2692dbfce569725b33ffb0a52d8de5ae8f0f0f66
Henrik Sjöstrand <henrik@hencjo.com>
2017-02-12 14:00:49 +0100

    Redundant keywords.

--- src/main/java/com/hencjo/summer/security/api/Responder.java
+++ src/main/java/com/hencjo/summer/security/api/Responder.java
@@ -5,8 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Responder {
-	public static enum ContinueOrRespond { RESPOND, CONTINUE; }
+	enum ContinueOrRespond { RESPOND, CONTINUE; }
 
-	public ContinueOrRespond respond(HttpServletRequest request, HttpServletResponse response) throws IOException;
+	ContinueOrRespond respond(HttpServletRequest request, HttpServletResponse response) throws IOException;
 	String describer();
 }
